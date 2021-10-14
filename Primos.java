fceefca545792aff2f0d802d44216cfdbd10dc42
Jessica Toyota <59458333+jeytoyota@users.noreply.github.com>
2021-10-14 20:06:44 -0300

    Update Primos.java

--- Primos.java
+++ Primos.java
@@ -2,7 +2,7 @@
 public class Primos {
 	
 	public static void main(String[] args) {    
-        for (int i = 2; i <= 100; i++) {
+        for (int i = 2; i <= 200; i++) {
             if( ehPrimo(i) )
                 System.out.println(i + " e primo.");
         }    
